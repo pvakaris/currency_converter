7d5fc29d2314d6470ffb95b522ea52fc81f26bfe
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2022-03-16 11:28:32 +0000

    Initial commit

---
+++ src/test/java/com/example/currency_converter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currency_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
